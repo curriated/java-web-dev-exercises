9eae9de18f2b1f13bf3b80deb3ef2fe97f41c622
Maddi <curriated@gmail.com>
2022-01-03 22:11:39 -0600

    Class 1 Exercises and Studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,33 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius of your circle?");
+        String userInput = input.nextLine();
+        double radius = -1;
+
+        try {
+            radius = Double.parseDouble(userInput);
+            while (radius < 0) {
+                System.out.println("Please enter a number greater than zero.");
+                System.out.println("What is the radius of your circle?");
+                userInput = input.nextLine();
+                radius = Double.parseDouble(userInput);
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a numeric value");
+            System.out.println("What is the radius of your circle?");
+            userInput = input.nextLine();
+        }
+
+        double area = Circle.getArea(radius);
+        System.out.println(area);
+
+
+    }
+}
