2b9e2826b4381f26bc68ed598b74d862b28ff00b
Maddi <curriated@gmail.com>
2022-01-06 20:14:39 -0600

    Counting Characters studio - version with user input

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -1,26 +1,31 @@
 package org.launchcode.java.studios.countingcharacters;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args) {
-        String paragraph = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        char[] charactersInString = paragraph.toLowerCase().toCharArray();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter some text:");
+        String paragraph = input.nextLine();
+        if(paragraph.isEmpty()) {
+            System.out.println("Please actually enter something you heathen.");
+        } else {
+            char[] charactersInString = paragraph.toLowerCase().toCharArray();
 
-        HashMap<Character, Integer> alphabet = new HashMap<>();
+            HashMap<Character, Integer> alphabet = new HashMap<>();
 
-        for (char c: charactersInString) {
-            if(Character.isLetter(c)) {
-                if (!alphabet.containsKey(c)) {
-                    alphabet.put(c, 1);
-                } else {
-                    int count = alphabet.get(c) + 1;
-                    alphabet.replace(c, count);
+            for (char c : charactersInString) {
+                if (Character.isLetter(c)) {
+                    if (!alphabet.containsKey(c)) {
+                        alphabet.put(c, 1);
+                    } else {
+                        int count = alphabet.get(c) + 1;
+                        alphabet.replace(c, count);
+                    }
                 }
             }
+            System.out.println(alphabet);
         }
-
-        System.out.println(alphabet);
-
     }
 }
